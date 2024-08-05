555b19194f27ae0d7434ee4a7ef3a54dbcb0969f
danylyoenko <lockmaster19@gmail.com>
2024-08-05 13:26:24 +0300

    create new button

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,15 @@
 const box = document.getElementById("box");
 const heart = document.getElementById("heart");
+const title = document.getElementById("title");
 
 box.addEventListener("click", function () {
   box.classList.add("hidden");
+  title.style.display = "none";
   heart.style.display = "flex";
 });
 
 heart.addEventListener("click", function () {
   heart.style.display = "none";
   box.classList.remove("hidden");
+  title.style.display = "block";
 });
